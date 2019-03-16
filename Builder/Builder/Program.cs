447228a0fa8c37c50090412da26265bf1918811d
u n 4 c k n 0 w l 3 z <gh0stsync.rt@gmail.com>
2019-03-16 15:48:35 +0700

    Add files via upload

---
+++ Builder/Builder/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream fileStream = new FileStream("Stub.exe", FileMode.Open, FileAccess.ReadWrite, FileShare.None))
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                {
+                    fileStream.Position = fileStream.Length + 1;
+                    binaryWriter.Write("*** Hello World ***");
+                }
+            }
+        }
+    }
+}
---
+++ Stub/Stub/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Stub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader streamReader = new StreamReader(System.Reflection.Assembly.GetEntryAssembly().Location))
+            {
+                using (BinaryReader binaryReader = new BinaryReader(streamReader.BaseStream))
+                {
+                    byte[] stubBytes = binaryReader.ReadBytes(Convert.ToInt32(streamReader.BaseStream.Length));
+                    string stubSettings = Encoding.ASCII.GetString(stubBytes).Substring(Encoding.ASCII.GetString(stubBytes).IndexOf("***")).Replace("***", "");
+                    Console.WriteLine(stubSettings);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
